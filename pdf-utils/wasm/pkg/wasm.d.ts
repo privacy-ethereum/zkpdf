0abd006b9e6466e4e52373635328b6c2e44f7dc2
0xvikasrushi <0xvikas@gmail.com>
2025-06-05 03:20:53 +0530

    feat: wasm export

---
+++ pdf-utils/wasm/pkg/wasm.d.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * WebAssembly export: verify text and signature in a PDF
+ */
+export function wasm_verify_text(pdf_bytes: Uint8Array, page_number: number, sub_string: string, position: number): boolean;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly wasm_verify_text: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
--- pdf-utils/wasm/src/lib.rs
+++ pdf-utils/wasm/src/lib.rs
@@ -1,6 +1,8 @@
 use core::verify_text;
+// use extractor::extract_text;
 use wasm_bindgen::prelude::*;
 
+/// WebAssembly export: verify text and signature in a PDF
 #[wasm_bindgen]
 pub fn wasm_verify_text(
     pdf_bytes: &[u8],
@@ -10,3 +12,12 @@ pub fn wasm_verify_text(
 ) -> bool {
     verify_text(pdf_bytes.to_vec(), page_number, sub_string, position).unwrap_or(false)
 }
+
+// /// WebAssembly export: extract raw text content per page
+// #[wasm_bindgen]
+// pub fn wasm_extract_text(pdf_bytes: &[u8]) -> Vec<JsValue> {
+//     match extract_text(pdf_bytes.to_vec()) {
+//         Ok(pages) => pages.into_iter().map(JsValue::from).collect(),
+//         Err(_) => Vec::new(),
+//     }
+// }
